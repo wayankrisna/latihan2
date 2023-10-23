d547b627f6f458a688f19aebdde42ffe373a9079
wayankrisna <krisnayasa2018@gmail.com>
2023-10-23 20:50:06 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int bil1, bil2;
+    cout << "======= VARIABEL =======" <<endl <<endl;
+    cout <<"Masukkan Bilangan 1 : " <<endl;
+    cin >> bil1;
+    cout <<"Masukkan Bilangan 2 : " <<endl;
+    cin >> bil2;
+    cout <<"------------------------" <<endl;
+    cout <<"Hasil : " <<endl;
+    cout <<"Bilangan 1 = " <<bil1 <<endl;
+    cout <<"Bilangan 2 = " <<bil2 <<endl;
+    cout <<"------------------------" <<endl;
+
+    return 0;
+}
